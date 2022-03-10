53372884399c1bde862e146ffc95aee032b11785
Kazuki Hashimoto <kazukih@tuta.io>
2022-03-11 01:08:14 +0900

    disp: msm: dsi: Don't busy wait
    That's a LONG wait. Don't busy wait there to save power.
    
    Signed-off-by: Kazuki Hashimoto <kazukih@tuta.io>
    Signed-off-by: Alexander Winkowski <dereference23@outlook.com>

--- drivers/gpu/drm/msm/dsi-staging/dsi_display.c
+++ drivers/gpu/drm/msm/dsi-staging/dsi_display.c
@@ -7459,7 +7459,7 @@ static void dsi_display_handle_fifo_overflow(struct work_struct *work)
 	 * Add sufficient delay to make sure
 	 * pixel transmission has started
 	 */
-	udelay(200);
+	usleep_range(180, 220);
 end:
 	dsi_display_clk_ctrl(display->dsi_clk_handle,
 			DSI_ALL_CLKS, DSI_CLK_OFF);
@@ -7537,7 +7537,7 @@ static void dsi_display_handle_lp_rx_timeout(struct work_struct *work)
 	 * Add sufficient delay to make sure
 	 * pixel transmission as started
 	 */
-	udelay(200);
+	usleep_range(180, 220);
 
 end:
 	dsi_display_clk_ctrl(display->dsi_clk_handle,
